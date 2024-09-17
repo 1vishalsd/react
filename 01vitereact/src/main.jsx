8482434a63a5791d1eb6e407fe09f65295466351
1VishalSD <ilearnvk@gmail.com>
2024-09-17 05:58:11 +0530

    how use the custom react in react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import React from "react";
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React !</h1>
+    </div>
+  );
+}
+/*
+const ReactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: " Click me to visit google",
+};
+*/
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "Click Me to visit google"
+);
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+createRoot(document.getElementById("root")).render(<App />);
