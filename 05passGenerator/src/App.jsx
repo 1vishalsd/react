cb5bce5e538908ef453c2d82e7aab6241dda8b20
1VishalSD <ilearnvk@gmail.com>
2024-09-19 05:21:49 +0530

    write a logic about pass

--- 05passGenerator/src/App.jsx
+++ 05passGenerator/src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 function App() {
   const [length, setlength] = useState(8);
   const [numAllowed, setNumAllowe] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
-  return <h1 className="text-white">Password Generator</h1>;
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+    if (numAllowed) str += "0123456789";
+    if (charAllowed) str += "!@##$%^&*()_+|~?";
+
+    for (let i = 1; i <= Array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
+  }, [numAllowed, charAllowed, setPassword]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto  shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700"></div>
+    </>
+  );
 }
 
 export default App;
